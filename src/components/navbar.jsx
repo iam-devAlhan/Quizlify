81241c0e698fcf81dbeaebbb0d6531181a3cc518
Alhaanahmed-duet <Alhaanahmed123@gmail.com>
2024-06-09 18:40:37 +0500

    Navbar responsive done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,16 @@
 import styles from './css/nav.module.css';
-
+import { useState } from 'react';
 
 const Navbar = function () {
+    const [navToggle, setNavToggle] = useState({isToggled: false, toggled: ''})
+    
+    function toggleMobileMenu() {
+        setNavToggle((prev) => ({
+            isToggled: !prev.isToggled,
+            toggled: prev.isToggled ? styles.toggler : ''
+        }))
+    }
+
     return (
         <>
             <header>
@@ -9,11 +18,23 @@ const Navbar = function () {
                     <div className={styles.container}>
                     <h1>LOGO</h1>
                         <ul>
-                            <li><a>FAQ</a></li>
-                            <li><a>Donate Us</a></li>
-                            <li><a>Sign in</a></li>
+                            <li id={styles['nav-link']}><a>FAQ</a></li>
+                            <li id={styles['nav-link']}><a>Donate Us</a></li>
+                            <li id={styles['nav-link']}><a>Sign in</a></li>
+                        </ul>
+                        
+                        <button id={styles.toggleNav} onClick={toggleMobileMenu}>{navToggle.isToggled ? <i className="bi bi-list"></i> : <i className="bi bi-x-lg"></i>}</button>
+                    </div>
+                    
+                    
+                    <div className={`${styles.mobileView} ${navToggle.toggled}`}>
+                        <ul>
+                        <li id={styles['nav-link']}><a>FAQ</a></li>
+                        <li id={styles['nav-link']}><a>Donate Us</a></li>
+                        <li id={styles['nav-link']}><a>Sign in</a></li>                            
                         </ul>
                     </div>
+
                 </nav>
             </header>
         </>
