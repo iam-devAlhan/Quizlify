22d05cd2b175d9639c632a84534ebbb1d5269590
AlhaanAhmed-duet <Alhaanahmed123@gmail.com>
2024-09-20 10:04:29 +0500

    feew! Dashboard completed :D

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,27 @@
 import styles from "../pages/css/signup.module.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 const Login = function() {
+    let user = {
+        name: "admin",
+        password: "admin123",
+        email: "admin@example.com"
+    }
+    const [newUser, setNewUser] = useState({
+        name: "",
+        password: "",
+        email: ""
+    })
+    const navigate = useNavigate();
     const submitHandler = function(event) {
         event.preventDefault();
+        if (newUser.name === user.name && newUser.email === user.email && newUser.password === user.password) {
+            navigate('/dashboard');
+        }
+        else {
+            alert("Wrong Credentials, try again")
+        }
+        
     }
     return (
         <>
@@ -9,9 +29,11 @@ const Login = function() {
                 <div className={styles.container}>
                 <h1 id={styles.heading}>Login into your account</h1>
                     <form id={styles.form}>
-                        <input id={styles.input} type="text" placeholder="Username" required/>
-                        <input id={styles.input} type="email" placeholder="username@example.com" required />
-                        <input id={styles.input} type="password" placeholder="Enter your password" required />
+                        <input id={styles.input} type="text" placeholder="Username" required onChange={(e) => setNewUser((prev) => {
+                            return {...prev, name: e.target.value}
+                        })}/>
+                        <input id={styles.input} type="email" placeholder="username@example.com" required onChange={(e) => setNewUser((prev) => ({...prev, email: e.target.value}))}/>
+                        <input id={styles.input} type="password" placeholder="Enter your password" required onChange={(e) => setNewUser((prev) => ({...prev, password: e.target.value}))}/>
                         <button id={styles.button} type="submit" onClick={submitHandler}>Sign In</button>
                     </form>
                 </div>
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,9 +1,44 @@
-const Dashboard = () => {
+import { useState } from "react";
+import styles from "../pages/css/dashboard.module.css";
+
+export default function Dashboard() {
+
+    const [isSidebarOn, setIsSidebarOn] = useState(false);
+    
+    const clickToggleSidebar = function() {
+            setIsSidebarOn(prev => !prev);
+    }
+
     return (
         <>
-            <div>Dashboard</div>
+        <div className={styles.container}>
+            {/* For Sidebar */}
+            <div className={styles.sidebar} style={{width: isSidebarOn ? "230px" : "70px"}}>
+                <button id={styles.btn} onClick={clickToggleSidebar}><i className="bi bi-list"></i></button>
+                <ul className={styles.menuList} style={{display: isSidebarOn ? "block" : "none"}}>
+                    <li className={styles.menuItem}><i className="bi bi-house-fill"></i> Home</li>
+                    <li className={styles.menuItem}><i className="bi bi-people-fill"></i> Groups</li>
+                    <li className={styles.menuItem}><i className="bi bi-person-workspace"></i> Workspace</li>
+                    <li className={styles.menuItem}><i className="bi bi-award-fill"></i> Your Grades</li>   
+                </ul>
+                <ul className={styles.propList} style={{display: isSidebarOn ? "block" : "none"}}>
+                    <li className={styles.propItem}><i className="bi bi-person-circle"></i> <span style={{fontSize: "15px"}}>Profile</span></li>
+                    <li className={styles.propItem}><i className="bi bi-chat-square"></i> <span style={{fontSize: "15px"}}>Notifications</span></li>
+                    <li className={styles.propItem}><i className="bi bi-box-arrow-left"></i> <span style={{fontSize: "15px"}}>Logout</span></li>
+                </ul>
+            </div>
+              
+            {/* For Main Content */}
+            <main id={styles.mainContent}>
+                <div>
+                    <h1>Dashboard</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quidem doloremque sint distinctio, totam, quibusdam dignissimos mollitia corporis repellendus voluptatum quis odio illum, veniam aut optio necessitatibus! Aliquid, expedita nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt iure harum illo consequatur perspiciatis reiciendis animi quos dolores sequi, culpa quae facilis deserunt et ducimus maxime aspernatur eius placeat qui!</p>
+                </div>
+            </main>
+        
+        </div>
+
+        
         </>
     )
 }
-
-export default Dashboard;
